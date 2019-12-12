770b9af0873fd252a3fc81a02434f63d7afab499
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 12:39:17 +0530

    Zuul api gateway is added.

---
+++ src/main/java/com/gatewayService/E_Gatewayservice/EGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gatewayService.E_Gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatewayService/E_Gatewayservice/EGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewayService.E_Gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
