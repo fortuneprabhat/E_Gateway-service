fb2e47946ee6e2e01ddbc12f19ff49d82779ee68
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 16:56:01 +0530

    filter is added on parameter.

--- src/main/java/com/gatewayService/E_Gatewayservice/EGatewayServiceApplication.java
+++ src/main/java/com/gatewayService/E_Gatewayservice/EGatewayServiceApplication.java
@@ -4,14 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.gatewayService.E_Gatewayservice.filters.AddRequestHeaderFilters;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@Configuration
 public class EGatewayServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EGatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public AddRequestHeaderFilters addRequestHeaderFilters() {
+		return new AddRequestHeaderFilters();
+	}
 }
